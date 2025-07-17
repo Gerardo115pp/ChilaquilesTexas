03088c6d404fa4be0dc598813f2ff6283e15117d
Gerardo <theronin115@gmail.com>
2025-07-17 05:05:36 -0600

    feat: created the fundational skeleton to start the website's development

---
+++ chilaquiles_texas_webclient/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '@app/app.css';
+</script>
+
+<div id="libery-website-content">
+    <slot></slot>
+</div>
--- chilaquiles_texas_webclient/svelte.config.js
+++ chilaquiles_texas_webclient/svelte.config.js
@@ -6,7 +6,23 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			"@errors/*": "./src/errors/*",
+			"@libs/*": "./src/libs/*",
+			"@components/*": "./src/components/*",
+			"@models/*": "./src/models/*",
+			"@pages/*": "./src/pages/*",
+			"@svg/*": "./src/svg/*",
+			"@actions/*": "./src/actions/*",
+			"@events/*": "./src/events/*",
+			"@stores/*": "./src/stores/*",
+			"@databases/*": "./src/databases/*",
+			"@themes/*": "./src/themes/*",
+			"@app/*": "./src/*",
+			"@config/*": "./src/config/*",
+			"@common/*": "./src/common/*",
+		}
 	}
 };
 
