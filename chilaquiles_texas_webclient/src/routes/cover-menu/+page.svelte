d0ef076aef3a0c51b38bfbdc90eb05cc0bde418b
Gerardo <theronin115@gmail.com>
2025-07-25 07:40:33 -0600

    feat: add cover menu page

---
+++ chilaquiles_texas_webclient/src/routes/cover-menu/+page.svelte
@@ -0,0 +1,35 @@
+<!-- 
+    'The blade must never be restrained.’He spoke the next sentence like a vow. ‘We shall always be the scythe.’
+
+    Perhaps we are drawn to campaigns that require us. Maybe there were worlds where Vulkan's concerns were the correct ones. Where Sanguinius'
+    nobility was needed.
+        'Not the worlds that fall in my path.' He thought of the words he had spoken
+        to the newly unified Death Guard. 'Doom shall stalk a thousand worlds.'
+        It would. Fate decreed it.
+
+    Banish all thoughts of mercy, because mercy is the plaything of the coward and the lie of the tyrant.
+
+    - Mortarion, The Pale king. Doom of tyrants.
+-->
+<main id="txc-cover-menu-page">
+    <article id="txc-cm-content" class="txc-common-main-content-layout">
+        <p class="txc-cm-content-p main-content-paragraph">
+            We named the place<br/>
+            after chilaquiles for a reason.
+        </p>
+        <p class="txc-cm-content-p main-content-paragraph">
+            Bold, soulful, made close to home,<br/>
+            and never tied to a schedule.
+        </p>
+        <p class="txc-cm-content-p main-content-paragraph">
+            Everything we serve shares that spirit:<br/>
+            comfort, flavor, memory,<br/>
+            and a little beautiful chaos.
+        </p>
+        <ul class="txc-ctas-container">
+            <button class="txc-button">
+                Menu & Order Online
+            </button>
+        </ul>
+    </article>
+</main>
