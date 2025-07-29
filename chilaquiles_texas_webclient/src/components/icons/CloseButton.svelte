8519c38029f9a3e58d9aa41964c6c9f9cfba79c0
Gerardo <theronin115@gmail.com>
2025-07-29 06:19:45 -0600

    feat: add CloseButton component

---
+++ chilaquiles_texas_webclient/src/components/icons/CloseButton.svelte
@@ -0,0 +1,15 @@
+<svg viewBox="0 0 60 60" class="txc-close-btn">
+    <path d="M4 4L56 56M4 56L56 4"/>
+</svg>
+
+<style>
+    svg.txc-close-btn {
+        /* overflow: visible; */
+        width: 100%;
+        aspect-ratio: 1 / 1;
+        stroke: var(--grey-1);
+        stroke-width: 7px;
+        fill: none;
+        stroke-linecap: round;
+    }
+</style>
