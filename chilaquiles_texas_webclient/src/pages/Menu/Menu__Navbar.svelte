9776b9246357c7fad215a8719cf1d569e54a856e
Gerardo <theronin115@gmail.com>
2025-10-12 01:27:04 -0600

    feat: finished desktop desing of the navbar from the menu page

--- chilaquiles_texas_webclient/src/pages/Menu/Menu__Navbar.svelte
+++ chilaquiles_texas_webclient/src/pages/Menu/Menu__Navbar.svelte
@@ -1,6 +1,9 @@
 <script>
     import { website_page_paths } from "@app/common/page_paths";
     import LosChilaquilesLogo from "@components/icons/LosChilaquilesLogo.svelte";
+    import AddressLabel from "@components/InformationElements/AddressLabel.svelte";
+    import OpenHoursLabel from "@components/InformationElements/OpenHoursLabel.svelte";
+    import PhoneLabel from "@components/InformationElements/PhoneLabel.svelte";
 
     
     /*=============================================
@@ -28,17 +31,30 @@
 </script>
 
 <nav id="txc-ic-menu-navbar">
-    <button class="rmp-mn-locale-option" class:active-locale={current_locale === 'en'} onclick={() => onLocaleChange('en')}>
-        English
-    </button>
-    <div id="rmp-mn-logo-wrapper">
-        <a href="{website_page_paths.HOME.path}">
-            <LosChilaquilesLogo />
-        </a>
+    <div id="txc-ic-menu-content--left">
+        <button class="rmp-mn-locale-option" class:active-locale={current_locale === 'es'} onclick={() => onLocaleChange('es')}>
+            Menú Español
+        </button>
+        <button class="rmp-mn-locale-option" class:active-locale={current_locale === 'en'} onclick={() => onLocaleChange('en')}>
+            English Menu
+        </button>
     </div>
-    <button class="rmp-mn-locale-option" class:active-locale={current_locale === 'es'} onclick={() => onLocaleChange('es')}>
-        Español
-    </button>
+    <div id="txc-ic-menu-content--center">
+        <div class="txc-ic-mc-cc-content-item">
+            <AddressLabel />
+        </div>
+        <div id="rmp-mn-logo-wrapper" class="txc-ic-mc-cc-content-item">
+            <a href="{website_page_paths.HOME.path}">
+                <LosChilaquilesLogo />
+            </a>
+        </div>
+        <div class="txc-ic-mc-cc-content-item">
+            <OpenHoursLabel />
+        </div>
+    </div>
+    <div id="txc-ic-menu-content--right">
+        <PhoneLabel />
+    </div>
 </nav>
 
 <style>
@@ -46,39 +62,71 @@
         width: 100%;
         height: 100%;
         display: flex;
-        justify-content: center;
-        column-gap: var(--spacing-2);
+        container-type: size;
+        justify-content: space-between;
         align-items: center;
+        font-size: min(calc(var(--font-size-1) * .95), .75dvw);
     }
-
-    button.rmp-mn-locale-option {
-        font-family: var(--font-read);
-        background: transparent;
-        border: none;
-        color: hsl(from var(--theme-color) h s calc(l * 0.8) / 1);
-        text-transform: none;
-        font-size: var(--font-size-1);
-        transition: color 0.3s ease-out, scale 0.1s ease-out;
-        
-        &.active-locale {
-            font-weight: bolder;
-            text-decoration: underline;
+    
+    
+    /*----------  Left content  ----------*/
+    
+        #txc-ic-menu-content--left {
+            display: flex;
         }
-        
-        &:not(.active-locale):hover {
-            cursor: pointer;
-            color: hsl(from var(--theme-color) h 100% l / 1);
-            transform-origin: center;
-            scale: 1.03;
-        }
-    }
 
-    #rmp-mn-logo-wrapper {
-        width: min(200px, 9.5%);
-        height: max-content;
+        button.rmp-mn-locale-option {
+            font-family: var(--font-read);
+            background: transparent;
+            border: none;
+            color: var(--body-fg-color);
+            text-transform: none;
+            font-size: 1em;
+            transition: color 0.3s ease-out, scale 0.1s ease-out;
+            padding-block: 0;
+            padding-inline: 1em;
+            
+            &.active-locale {
+                font-weight: bolder;
+                text-decoration: underline;
+                color: hsl(from var(--theme-color) h s calc(l * 0.8) / 1);
+            }
 
-        & > a {
-            width: 100%;
+            &:not(.active-locale):hover {
+                cursor: pointer;
+                color: hsl(from var(--theme-color) h 100% l / 1);
+                transform-origin: center;
+                scale: 1.03;
+            }
+
+            &:not(:last-child) {
+                border-right: 1px solid var(--body-fg-color);
+            }
         }
-    }
+
+    
+    /*----------  Center Content  ----------*/
+
+        #txc-ic-menu-content--center {
+            display: flex;
+            align-items: center;
+            gap: 3em;
+        }
+
+        #rmp-mn-logo-wrapper {
+            width: min(200px, 9.5cqw);
+            height: max-content;
+
+            & > a {
+                width: 100%;
+            }
+        }
+
+    
+    /*----------  Right Content  ----------*/
+        #txc-ic-menu-content--right {
+            font-size: 1.2em;
+        }
+    
+    
 </style>
